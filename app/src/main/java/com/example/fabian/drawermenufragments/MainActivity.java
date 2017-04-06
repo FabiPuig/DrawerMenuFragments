edc10bb492b2014ddd150ae3a7ebd06e50202bd7
Fabian <20464654j@iespoblenou.org>
2017-04-06 12:03:15 +0200

    Gestionamos el drawer en nuestro activity ( tambien el icono del menu)

--- app/src/main/java/com/example/fabian/drawermenufragments/MainActivity.java
+++ app/src/main/java/com/example/fabian/drawermenufragments/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.fabian.drawermenufragments;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawer;
+    private Toolbar toolbar;
+    private NavigationView nvDrawer;
+
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Reemplazamos el ActionBar por la Toolbar
+        toolbar = (Toolbar) findViewById( R.id.toolbar );
+        setSupportActionBar( toolbar );
+
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // La acción home/up de la barra de acción debe abrir o cerrar el drawer
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawer.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
