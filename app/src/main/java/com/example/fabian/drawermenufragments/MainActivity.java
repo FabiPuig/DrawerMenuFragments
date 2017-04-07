80828cfefc68c626d31a4dab1a890a1909ae334c
Fabian <20464654j@iespoblenou.org>
2017-04-07 09:42:08 +0200

    Gestionamos la navegacion entre menu items usando el FragmentManager

--- app/src/main/java/com/example/fabian/drawermenufragments/MainActivity.java
+++ app/src/main/java/com/example/fabian/drawermenufragments/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.fabian.drawermenufragments;
 
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -11,9 +13,9 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    private DrawerLayout mDrawer;
+    private DrawerLayout drawerLayout;
     private Toolbar toolbar;
-    private NavigationView nvDrawer;
+    private NavigationView navigationView;
 
     private ActionBarDrawerToggle drawerToggle;
 
@@ -26,20 +28,72 @@ public class MainActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById( R.id.toolbar );
         setSupportActionBar( toolbar );
 
-        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerLayout = (DrawerLayout) findViewById( R.id.drawer_layout );
+
+        // Preparamos el drawer view
+        navigationView = (NavigationView) findViewById( R.id.nvView );
+        setupDrawerContent( navigationView );
 
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected( MenuItem item ) {
 
         // La acción home/up de la barra de acción debe abrir o cerrar el drawer
-        switch (item.getItemId()) {
+        switch ( item.getItemId() ) {
             case android.R.id.home:
-                mDrawer.openDrawer(GravityCompat.START);
+                drawerLayout.openDrawer( GravityCompat.START );
                 return true;
         }
 
-        return super.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected( item );
+    }
+
+    private void setupDrawerContent( NavigationView navigationView ) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected( MenuItem menuItem ) {
+                        selectDrawerItem( menuItem );
+                        return true;
+                    }
+                });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        // Crea un nuevo fragment y especifica el fragment a mostrar basandose en el item
+        // del nav seleccionado
+        Fragment fragment = null;
+        Class fragmentClass;
+        switch(menuItem.getItemId()) {
+            case R.id.nav_first_fragment:
+                fragmentClass = FirstFragment.class;
+                break;
+            case R.id.nav_second_fragment:
+                fragmentClass = SecondFragment.class;
+                break;
+            case R.id.nav_third_fragment:
+                fragmentClass = ThirdFragment.class;
+                break;
+            default:
+                fragmentClass = FirstFragment.class;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Inserta el fragment reemplazando el existente
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+        // Realza el item seleccionado en el NavigationView
+        menuItem.setChecked(true);
+        // Añadimos el titulo a la toolbar
+        setTitle(menuItem.getTitle());
+        // Cierra el navigation drawer
+        drawerLayout.closeDrawers();
     }
 }
